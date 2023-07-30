df9aae06ec153d8d440f65f1d129078765f9195b
Sujal Jain <ethicalwolf333@gmail.com>
2023-07-30 21:50:26 +0530

    Added the two functions' declarations in parser/parser.go

--- parser/parser.go
+++ parser/parser.go
@@ -9,12 +9,20 @@ import (
 
 type Parser struct {
 	l *lexer.Lexer
+	errors []string
 
 	curToken token.Token
 	peekToken token.Token
 
-	errors []string
+	prefixParseFns map[token.TokenType]prefixParseFn
+	infixParseFns map[token.TokenType]infixParseFn
 }
+
+type (
+	prefixParseFn func() ast.Expression
+	infixParseFn func(ast.Expression) ast.Expression // this function takes an expression which is on the left side of the operator
+)
+
 
 func New(l *lexer.Lexer) *Parser {
 	p := &Parser{l : l, errors: []string{}}
