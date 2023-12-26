1d41643e1190ae19a98eea4486645b904fd3fa3e
sujalcodes3 <ethicalwolf333@gmail.com>
2023-12-27 02:40:11 +0530

    Studied the lexer.go file, and commented the file.

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -2,6 +2,7 @@ package lexer
 
 import "monkeylang/token"
 
+// our lexer has the input string(which the source code writter by the user).
 type Lexer struct {
 	input        string
 	position     int  // current position
@@ -9,62 +10,67 @@ type Lexer struct {
 	ch           byte // current character
 }
 
+// basically a constructor of our lexer.
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
 	l.readChar()
 	return l
 }
 
+// returns the next token in the input source code.
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
+	// skips all types of whitespaces in our source code before giving the next token.
 	l.skipWhiteSpace()
 
+	// checks the type of the current character and returns the corresponding token.
 	switch l.ch {
 	case '=':
+		// checks if the next character is '=' and if so, returns the token for '=='.
 		if l.peekChar() == '=' {
 			ch := l.ch
 			l.readChar()
-			tok = token.Token{Type: token.EQ, Literal: string(ch) + string(l.ch)}
-		} else {
-			tok = newToken(token.ASSIGN, l.ch)
+			tok = token.Token{Type: token.EQ, Literal: string(ch) + string(l.ch)} // this is the equality operator.
+		} else { // otherwise, returns the token for '='.
+			tok = newToken(token.ASSIGN, l.ch) // this is the assignment operator.
 		}
 	case '+':
-		tok = newToken(token.PLUS, l.ch)
+		tok = newToken(token.PLUS, l.ch) // normal stuff
 	case '-':
-		tok = newToken(token.MINUS, l.ch)
+		tok = newToken(token.MINUS, l.ch) // normal stuff
 	case '!':
-		if l.peekChar() == '=' {
+		if l.peekChar() == '=' { // checks for the not equal operator.
 			ch := l.ch
 			l.readChar()
 			tok = token.Token{Type: token.NOT_EQ, Literal: string(ch) + string(l.ch)}
 		} else {
-			tok = newToken(token.BANG, l.ch)
+			tok = newToken(token.BANG, l.ch) // checks for the "bang" operator.
 		}
 	case '/':
-		tok = newToken(token.SLASH, l.ch)
+		tok = newToken(token.SLASH, l.ch) // normal stuff
 	case '*':
-		tok = newToken(token.ASTERISK, l.ch)
+		tok = newToken(token.ASTERISK, l.ch) // normal stuff
 	case '<':
-		tok = newToken(token.LT, l.ch)
+		tok = newToken(token.LT, l.ch) // normal stuff
 	case '>':
-		tok = newToken(token.GT, l.ch)
+		tok = newToken(token.GT, l.ch) // normal stuff
 	case ';':
-		tok = newToken(token.SEMICOLON, l.ch)
+		tok = newToken(token.SEMICOLON, l.ch) // normal stuff
 	case ',':
-		tok = newToken(token.COMMA, l.ch)
+		tok = newToken(token.COMMA, l.ch) // normal stuff
 	case '(':
-		tok = newToken(token.LPAREN, l.ch)
+		tok = newToken(token.LPAREN, l.ch) // normal stuff
 	case ')':
-		tok = newToken(token.RPAREN, l.ch)
+		tok = newToken(token.RPAREN, l.ch) // normal stuff
 	case '{':
-		tok = newToken(token.LBRACE, l.ch)
+		tok = newToken(token.LBRACE, l.ch) // normal stuff
 	case '}':
-		tok = newToken(token.RBRACE, l.ch)
-	case 0:
+		tok = newToken(token.RBRACE, l.ch) // normal stuff
+	case 0: // end of file
 		tok.Literal = ""
 		tok.Type = token.EOF
-	default:
+	default: // checks if the character is a letter or a digit.
 		if isLetter(l.ch) {
 			tok.Literal = l.readIdentifier()
 			tok.Type = token.LookupIdent(tok.Literal)
@@ -74,34 +80,38 @@ func (l *Lexer) NextToken() token.Token {
 			tok.Literal = l.readNumber()
 			return tok
 		} else {
-			tok = newToken(token.ILLEGAL, l.ch)
+			tok = newToken(token.ILLEGAL, l.ch) // illegal stuff.
 		}
 	}
-	l.readChar()
-	return tok
+	l.readChar() // moves to the next character.
+	return tok   // returns the token.
 }
 
+// creates a new token with the given type and literal.
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
 }
 
+// returns the identifier as a string from the input source code.
 func (l *Lexer) readIdentifier() string {
 	position := l.position
-	for isLetter(l.ch) {
-		l.readChar()
+	for isLetter(l.ch) { // checks that the character is a letter.
+		l.readChar() // advances our position in the input string.
 	}
-	return l.input[position:l.position]
+	return l.input[position:l.position] // returns the identifier as a string.
 }
 
+// returns the number as string.
 func (l *Lexer) readNumber() string {
-	position := l.position
+	position := l.position // save the position of the first digit.
 	for isDigit(l.ch) {
-		l.readChar()
+		l.readChar() // advance our position in the input string.
 	}
-	return l.input[position:l.position]
+	return l.input[position:l.position] // return the number as a string.
 }
 
-func (l *Lexer) readChar() { // give us the next character and advance to our position in the input
+// readChar advances our position in the input source code string
+func (l *Lexer) readChar() {
 	if l.readPosition >= len(l.input) {
 		l.ch = 0 // end of file, 0 is ASCII for NULL
 	} else {
@@ -112,6 +122,7 @@ func (l *Lexer) readChar() { // give us the next character and advance to our po
 	l.readPosition += 1
 }
 
+// used to peek at the next character without advancing our position in the input.
 func (l *Lexer) peekChar() byte {
 	if l.readPosition >= len(l.input) {
 		return 0
@@ -120,14 +131,15 @@ func (l *Lexer) peekChar() byte {
 	}
 }
 
+// the following two functions are helper functions to check if a character is a letter or a digit.
 func isLetter(ch byte) bool {
 	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
-
 func isDigit(ch byte) bool {
 	return '0' <= ch && ch <= '9'
 }
 
+// skips all types of whitespaces in our source code.
 func (l *Lexer) skipWhiteSpace() {
 	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
 		l.readChar()
