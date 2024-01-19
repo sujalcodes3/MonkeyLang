1a8f49ad07fac144d408d655fce697471498e118
sujalcodes3 <ethicalwolf333@gmail.com>
2024-01-19 19:09:10 +0530

    TASKS : done
    1. Boolean Evaluation
    2. NULL Evaluation
    3. prefix BANG and MINUS

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,13 @@ import (
 	"monkeylang/object"
 )
 
+var (
+    TRUE = &object.Boolean{Value: true}
+    FALSE = &object.Boolean{Value: false}
+
+    NULL = &object.Boolean{}
+)
+
 func Eval(node ast.Node) object.Object {
     switch node := node.(type) {
         // statements
@@ -12,12 +19,59 @@ func Eval(node ast.Node) object.Object {
             return evalStatements(node.Statements)
         case *ast.ExpressionStatement:
             return Eval(node.Expression)
+        case *ast.PrefixExpression:
+            right := Eval(node.Right)
+            return evalPrefixExpression(node.Operator, right)
         // expression
         case *ast.IntegerLiteral:
             return &object.Integer{Value: node.Value} // returns an integer object of our internal representation of our language.
+        case *ast.Boolean:
+            return nativeBoolToBooleanObject(node.Value) 
     }
 
     return nil
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+    switch operator {
+    case "!" :
+        return evalBangOperatorExpression(right)
+    case "-" :
+        return evalPrefixMinusOperator(right)
+    default:
+        return NULL
+    }
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+    switch right {
+    case TRUE:
+        return FALSE
+    case FALSE:
+        return TRUE 
+    case NULL:
+        return TRUE
+    default:
+        return FALSE
+    }
+}
+
+func evalPrefixMinusOperator(right object.Object) object.Object {
+    if(right.Type() != object.INTEGER_OBJ) {
+        return NULL
+    } 
+
+    value := right.(*object.Integer).Value
+
+    return &object.Integer{Value: -value}
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+    if input {
+        return TRUE
+    }
+
+    return FALSE
 }
 
 func evalStatements(stmts []ast.Statement) object.Object{
@@ -29,3 +83,4 @@ func evalStatements(stmts []ast.Statement) object.Object{
 
     return result
 }
+
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -7,6 +7,29 @@ import (
 	"testing"
 )
 
+func TestEvalBangOperator(t * testing.T) {
+    tests := [] struct {
+        input string
+        expected bool 
+    } {
+        {"!true", false},
+        {"!false", true},
+        {"!5", false},
+        {"!!true", true}, 
+        {"!!false", false},
+        {"!!5", true},
+    }
+    
+
+    for _, tt := range tests {
+        evaluated := testEval(tt.input)
+        testBooleanObject(t, evaluated, tt.expected)
+    }
+}
+
+
+
+// for integer expressions
 func TestEvalIntegerExpression(t * testing.T) {
     tests := []struct {
         input string
@@ -14,6 +37,8 @@ func TestEvalIntegerExpression(t * testing.T) {
     } {
         {"5", 5},
         {"4", 4},
+        {"-4", -4},
+        {"-5", -5},
     }
 
     for _, tt := range tests {
@@ -21,15 +46,25 @@ func TestEvalIntegerExpression(t * testing.T) {
         testIntegerObject(t, evaluated, tt.expected)
     }
 }
+// for boolean expressions
+func TestEvalBooleanExpression(t * testing.T) {
+    tests := []struct {
+        input string
+        expected bool
+    } {
+        {"true", true},
+        {"false", false},
+    }
 
-func testEval(input string) object.Object {
-    l := lexer.New(input)
-    p := parser.New(l)
-    program := p.ParseProgram()
+    for _, tt := range tests {
+        evaluated := testEval(tt.input) 
+        testBooleanObject(t, evaluated, tt.expected)
+    }
+}
 
-    return Eval(program)
-} 
 
+
+// for integer expressions
 func testIntegerObject(t * testing.T, evaluated object.Object, expected int64) bool {
     result, ok := evaluated.(*object.Integer)
 
@@ -45,3 +80,28 @@ func testIntegerObject(t * testing.T, evaluated object.Object, expected int64) b
 
     return true
 }
+
+// for boolean expressions
+func testBooleanObject(t * testing.T, evaluated object.Object, expected bool) bool {
+    result, ok := evaluated.(*object.Boolean)
+
+    if !ok {
+        t.Errorf("object is not a boolean, got= %T (%+v)", evaluated, evaluated)
+        return false
+    }
+
+    if result.Value != expected {
+        t.Errorf("object has wrong value, got=%t, want= %t", result.Value, expected) 
+        return false
+    }
+
+    return true
+}
+
+func testEval(input string) object.Object {
+    l := lexer.New(input)
+    p := parser.New(l)
+    program := p.ParseProgram()
+
+    return Eval(program)
+} 
